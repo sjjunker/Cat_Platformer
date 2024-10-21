ba0bc7de0a6ab1d79bcc7af80297780f14302e8a
Sandi Junker <sandi.junker@outlook.com>
2024-10-21 09:43:43 -0500

    Initial Commit

---
+++ Cat_PlatformerTests/Cat_PlatformerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_PlatformerTests.swift
+//  Cat_PlatformerTests
+//
+//  Created by Sandi Junker on 10/21/24.
+//
+
+import Testing
+@testable import Cat_Platformer
+
+struct Cat_PlatformerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
